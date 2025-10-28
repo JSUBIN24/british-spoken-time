c26206a5e5cfae6b13b57e3e037138d137efa160
subin <subinbenat@gmail.com>
2025-10-28 11:20:24 +0100

    Add SpokenTimeFormatter interface and BritishSpokenTimeFormatter stub

---
+++ british-spoken-time/src/main/java/com/andela/spokentime/strategy/BritishSpokenTimeFormatter.java
@@ -0,0 +1,10 @@
+package com.andela.spokentime.strategy;
+
+import com.andela.spokentime.dto.SpokenTimeResponse;
+
+public class BritishSpokenTimeFormatter implements SpokenTimeFormatter{
+    @Override
+    public SpokenTimeResponse format(String time) {
+        return new SpokenTimeResponse("Not implemented yet");
+    }
+}
