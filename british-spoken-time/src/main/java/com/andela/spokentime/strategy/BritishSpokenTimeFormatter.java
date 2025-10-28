22a1727a94c275fe98031e1c9a5a59812c09843b
subin <subinbenat@gmail.com>
2025-10-28 11:44:47 +0100

    Implemented midnight, noon and Basic 'o'clock case and validated invalid time format.

--- british-spoken-time/src/main/java/com/andela/spokentime/strategy/BritishSpokenTimeFormatter.java
+++ british-spoken-time/src/main/java/com/andela/spokentime/strategy/BritishSpokenTimeFormatter.java
@@ -1,10 +1,51 @@
 package com.andela.spokentime.strategy;
 
 import com.andela.spokentime.dto.SpokenTimeResponse;
+import com.andela.spokentime.exception.InvalidTimeFormatException;
 
-public class BritishSpokenTimeFormatter implements SpokenTimeFormatter{
+public class BritishSpokenTimeFormatter implements SpokenTimeFormatter {
     @Override
     public SpokenTimeResponse format(String time) {
+
+        if (time == null || !time.matches("\\d{4}")) {
+            throw new InvalidTimeFormatException("Time must be in HHmm 24-hour format");
+        }
+
+        String hourStr = time.substring(0, 2);
+        String minuteStr = time.substring(2, 4);
+
+        int hour = Integer.parseInt(hourStr);
+        int minute = Integer.parseInt(minuteStr);
+
+        if (hour < 0 || hour > 23 || minute < 0 || minute > 59) {
+            throw new InvalidTimeFormatException("Invalid hour or minute");
+        }
+
+        if (hour == 0 && minute == 0) {
+            return new SpokenTimeResponse("midnight");
+        }
+
+        if (hour == 12 && minute == 0) {
+            return new SpokenTimeResponse("noon");
+        }
+
+        if (minute == 0) {
+            return new SpokenTimeResponse(String.format("%s o'clock", convertHourToWords(hour)));
+        }
+
         return new SpokenTimeResponse("Not implemented yet");
+    }
+
+    private String convertHourToWords(int hour) {
+        int displayHour = hour % 12;
+        if (displayHour == 0) {
+            displayHour = 12;
+        }
+        String[] numbers = {
+                "twelve", "one", "two", "three", "four",
+                "five", "six", "seven", "eight", "nine",
+                "ten", "eleven"
+        };
+        return numbers[displayHour];
     }
 }
