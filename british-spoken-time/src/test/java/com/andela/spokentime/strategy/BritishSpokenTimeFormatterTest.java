57a7d1507b8939ded7ee1817dc2af001cda9be3b
subin <subinbenat@gmail.com>
2025-10-28 19:31:01 +0100

    Added CSV file to validate all possibilities.

--- british-spoken-time/src/test/java/com/andela/spokentime/strategy/BritishSpokenTimeFormatterTest.java
+++ british-spoken-time/src/test/java/com/andela/spokentime/strategy/BritishSpokenTimeFormatterTest.java
@@ -3,6 +3,7 @@ package com.andela.spokentime.strategy;
 import com.andela.spokentime.dto.SpokenTimeResponse;
 import com.andela.spokentime.exception.InvalidTimeFormatException;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -13,17 +14,35 @@ class BritishSpokenTimeFormatterTest {
     private final BritishSpokenTimeFormatter formatter = new BritishSpokenTimeFormatter();
 
     @ParameterizedTest
-    @CsvSource({
-            "0000, midnight",
-            "1200, noon",
-            "0100, one o'clock",
-            "1200, noon",
-            "2300, eleven o'clock"
-    })
+    @CsvFileSource(resources = "/british_spoken_time_full.csv")
     public void testValidTimes(String input, String expected) {
         SpokenTimeResponse response = formatter.format(input);
         assertEquals(expected, response.spokenTime());
     }
+    @ParameterizedTest
+    @CsvSource({
+            "0000, midnight",
+            "0100, one o'clock",
+            "0205, five past two",
+            "0310, ten past three",
+            "0415, quarter past four",
+            "0520, twenty past five",
+            "0625, twenty five past six",
+            "0632, six thirty two",
+            "0730, half past seven",
+            "0735, twenty five to eight",
+            "0840, twenty to nine",
+            "0945, quarter to ten",
+            "1050, ten to eleven",
+            "1155, five to twelve",
+            "1200, noon",
+            "2300, eleven o'clock"
+    })
+    public void testValidTimesInput(String input, String expected) {
+        SpokenTimeResponse response = formatter.format(input);
+        assertEquals(expected, response.spokenTime());
+    }
+
 
     @ParameterizedTest
     @ValueSource(strings = {"2400", "1260", "abcd", "123", "9999", ""})
