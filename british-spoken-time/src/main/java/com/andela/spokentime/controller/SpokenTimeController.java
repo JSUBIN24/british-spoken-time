7f0985b9ce513860f6672a84771e151e7dfc0094
subin <subinbenat@gmail.com>
2025-10-28 11:36:16 +0100

    Create SpokenTimeController with endpoint for time formatting

---
+++ british-spoken-time/src/main/java/com/andela/spokentime/controller/SpokenTimeController.java
@@ -0,0 +1,21 @@
+package com.andela.spokentime.controller;
+
+import com.andela.spokentime.dto.SpokenTimeResponse;
+import com.andela.spokentime.service.SpokenTimeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class SpokenTimeController   {
+
+
+    private final SpokenTimeService service;
+
+    public SpokenTimeController(SpokenTimeService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/spoken-time")
+    public SpokenTimeResponse getSpokenTime(@RequestParam String time, @RequestParam(defaultValue = "british") String locale){
+        return service.getSpokenTime(time,locale);
+    }
+}
