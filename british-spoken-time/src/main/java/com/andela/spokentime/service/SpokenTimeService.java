61b09bf99ee431b01cdfbd57decaf8650e965122
subin <subinbenat@gmail.com>
2025-10-28 11:28:34 +0100

    Add SpokenTimeService to coordinate formatter usage

---
+++ british-spoken-time/src/main/java/com/andela/spokentime/service/SpokenTimeService.java
@@ -0,0 +1,12 @@
+package com.andela.spokentime.service;
+
+import com.andela.spokentime.dto.SpokenTimeResponse;
+import com.andela.spokentime.factory.SpokenTimeFormatterFactory;
+import com.andela.spokentime.strategy.SpokenTimeFormatter;
+
+public class SpokenTimeService {
+    public SpokenTimeResponse getSpokenTime(String time, String locale) {
+        SpokenTimeFormatter formatter = SpokenTimeFormatterFactory.getFormatter(locale);
+        return formatter.format(time);
+    }
+}
