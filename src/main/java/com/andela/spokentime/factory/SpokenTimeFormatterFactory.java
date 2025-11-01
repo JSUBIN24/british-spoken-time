5ff9f711a4a937142f29156315dcc3239677af62
subin <subinbenat@gmail.com>
2025-11-01 17:11:08 +0100

    Introduced Factory Design pattern.

--- src/main/java/com/andela/spokentime/factory/SpokenTimeFormatterFactory.java
+++ src/main/java/com/andela/spokentime/factory/SpokenTimeFormatterFactory.java
@@ -3,14 +3,25 @@ package com.andela.spokentime.factory;
 import com.andela.spokentime.strategy.BritishSpokenTimeFormatter;
 import com.andela.spokentime.strategy.SpokenTimeFormatter;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
 public class SpokenTimeFormatterFactory {
+
+
+    public static final Map<String, Supplier<SpokenTimeFormatter>> FORMATTER = Map.of("british", BritishSpokenTimeFormatter::new);
+
     public static SpokenTimeFormatter getFormatter(String locale) {
         if (locale == null) {
             throw new IllegalArgumentException("Locale cannot be null");
         }
-        return switch (locale.toLowerCase()) {
-            case "british" -> new BritishSpokenTimeFormatter();
-            default -> throw new IllegalArgumentException("Unsupported locale: " + locale);
-        };
+
+        var formatterSupplier = FORMATTER.get(locale.toLowerCase());
+        if (formatterSupplier == null) {
+            throw new IllegalArgumentException("Unsupported locale: " + locale);
+        }
+        return formatterSupplier.get();
+
     }
 }
